5c2a35c770fcb5a4b02d1e0d2e79fbe663c1d125
Tomoki　Mori <TomokiMori1974@users.noreply.github.com>
2014-02-23 17:21:05 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -5,7 +5,7 @@
 
 const SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-const MaxDescription = 19; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+const MaxDescription = 11; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 const MaxMonth = 3;
 
